9b882d234ad3200e4ae56943b46328b8198e0be2
EstherrrrLiu <changliues@gmail.com>
2021-11-18 23:35:19 -0800

    Combined duplicated parts in subcomp_b

--- emissions_calculator/subcomp_b_process_emissions_factors.py
+++ emissions_calculator/subcomp_b_process_emissions_factors.py
@@ -1,17 +1,87 @@
 import pandas as pd
-import numpy as np
 
-from subcomp_a_organize_data import create_emissions_rates_df, create_DR_hours_df_dict,\
-    create_DR_potential_df_dict, create_product_info_df, runall
+from subcomp_a_organize_data import create_emissions_rates_df, create_DR_hours_df_dict
 
-from emissions_parameters import DIR_EMISSIONS_RATES, \
-    DIR_DR_POTENTIAL_HRS, EMISSIONS_SCENARIO_LIST, EMISSIONS_RATES_FILES, \
-    DR_NAME, DR_HRS_FILES, DR_POTENTIAL_FILES, SUBSET_PRODUCTS, DR_SEASONS, \
-    DAYS_IN_MONTH
+from emissions_parameters import EMISSIONS_SCENARIO_LIST, EMISSIONS_RATES_FILES, DAYS_IN_MONTH
 
-emissions_rates_df_out, DR_hours_df_dict_out, DR_potential_df_dict_out, DR_product_info_df_dict_out = runall()
+emissions_rates_df_out = create_emissions_rates_df()
+DR_hours_df_dict_out = create_DR_hours_df_dict()
 
+
+# This part output results for old bins
 def old_bins_winter():
+
+    df_ave = {}
+
+    former_month_day, dr_month_day, latter_month_day = \
+        get_former_same_latter_day(DR_hours_df_dict_out['oldbins_Winter'])
+
+    for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
+
+        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
+
+        if idx == 0:
+            data = emissions_rates_df_out
+
+            "Compute daily average"
+            df_ave[idx] = get_new_hour_id_ave(data, former_month_day, dr_month_day, latter_month_day, column_name)
+
+        else:
+            pass
+
+    return df_ave
+
+
+def old_bins_summer():
+
+    df_ave = {}
+
+    former_month_day, dr_month_day, latter_month_day = \
+        get_former_same_latter_day(DR_hours_df_dict_out['oldbins_Summer'])
+
+    for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
+
+        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
+
+        if idx == 0:
+            data = emissions_rates_df_out
+
+            "Compute daily average"
+            df_ave[idx] = get_new_hour_id_ave(data, former_month_day, dr_month_day, latter_month_day, column_name)
+
+        else:
+            pass
+
+    return df_ave
+
+
+def oldbins_all_year():
+
+    df_ave = {}
+
+    frames = [DR_hours_df_dict_out['oldbins_Winter'], DR_hours_df_dict_out['oldbins_Summer']]
+    dr_hours_df = pd.concat(frames)
+
+    former_month_day, dr_month_day, latter_month_day = get_former_same_latter_day(dr_hours_df)
+
+    for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
+
+        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
+
+        if idx == 0:
+            data = emissions_rates_df_out
+
+            "Compute daily average"
+            df_ave[idx] = get_new_hour_id_ave(data, former_month_day, dr_month_day, latter_month_day, column_name)
+
+        else:
+            pass
+
+    return df_ave
+
+
+# Get days with DR hours
+def get_former_same_latter_day(time_df):
 
     """
      For DR_hours_df_dict_out['']
@@ -19,10 +89,7 @@ def old_bins_winter():
      Select (sum>=1), got DR days!
     """
 
-    df_2 = {}
-
-    "Get days with DR hours"
-    df_1 = DR_hours_df_dict_out['oldbins_Winter'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
+    df_1 = time_df.groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
     df_1 = df_1[df_1['DVR'] >= 1]
 
     "Get same, former and latter days with DR hours and put into list"
@@ -68,220 +135,76 @@ def old_bins_winter():
     for (month, day) in zip(months, days):
         dr_month_day.append(month*100+day)
 
-    for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
-
-        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
-
-        if idx == 0:
-            data = emissions_rates_df_out
-            df_cp = data
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_same = df_cp[df_cp['month_day'].isin(dr_month_day)]
-            df_former = df_cp[(df_cp['Report_Hour'] >= 12) & (df_cp['month_day'].isin(former_month_day))]
-            df_latter = df_cp[(df_cp['Report_Hour'] <= 12) & (df_cp['month_day'].isin(latter_month_day))]
-
-            df_same['new_hourID'] = df_same['Report_Hour']
-            df_former['new_hourID'] = df_former['Report_Hour'] - 24
-            df_latter['new_hourID'] = df_latter['Report_Hour'] + 24
-
-            frames = [df_same, df_former, df_latter]
-
-            df_all = pd.concat(frames)
-
-            "Compute daily average"
-            df_2[idx] = df_all.groupby(['new_hourID'])[column_name].mean().reset_index()
-
-        else:
-            pass
-
-    return df_2
+    return former_month_day, dr_month_day, latter_month_day
 
 
-def old_bins_summer():
+# Get hourly average by new hour ID
+def get_new_hour_id_ave(data, former_month_day, dr_month_day, latter_month_day, column_name):
+    df_cp = data
 
-    df_2 = {}
+    "Combine month and day together"
+    df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
+
+    "Select DR days in emission rates dataset"
+    df_same = df_cp[df_cp['month_day'].isin(dr_month_day)]
+    df_former = df_cp[(df_cp['Report_Hour'] >= 12) & (df_cp['month_day'].isin(former_month_day))]
+    df_latter = df_cp[(df_cp['Report_Hour'] <= 12) & (df_cp['month_day'].isin(latter_month_day))]
+
+    df_same['new_hourID'] = df_same['Report_Hour']
+    df_former['new_hourID'] = df_former['Report_Hour'] - 24
+    df_latter['new_hourID'] = df_latter['Report_Hour'] + 24
+
+    frames = [df_same, df_former, df_latter]
+
+    df_all = pd.concat(frames)
+
+    "Compute daily average"
+    return df_all.groupby(['new_hourID'])[column_name].mean().reset_index()
+
+
+# This part output results for new bins
+
+
+def get_hour_ave(data, time_df, column_name):
+    df_cp = data
 
     "Get days with DR hours"
-    df_1 = DR_hours_df_dict_out['oldbins_Summer'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
+    df_1 = time_df.groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
     df_1 = df_1[df_1['DVR'] >= 1]
 
-    "Get same, former and latter days with DR hours and put into list"
-    days = []
-    months = []
-    former_days = []
-    former_months = []
-    latter_days = []
-    latter_months = []
+    "Combine month and day together"
+    df_1['month_day'] = df_1['Month']*100+df_1['Day']
+    df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
 
-    for row in df_1.iterrows():
-        month = row[1]['Month']
-        day = row[1]['Day']
-        days.append(day)
-        months.append(month)
+    "Select DR days in emission rates dataset"
+    df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
 
-        if day-1 > 0:
-            former_day = day-1
-            former_month = month
-        else:
-            former_day = DAYS_IN_MONTH[month-2]
-            former_month = month-1
-
-        if day+1 <= DAYS_IN_MONTH[month-1]:
-            latter_day = day+1
-            latter_month = month
-        else:
-            latter_day = 1
-            latter_month = month+1
-
-        former_days.append(former_day)
-        former_months.append(former_month)
-        latter_days.append(latter_day)
-        latter_months.append(latter_month)
-
-    former_month_day = []
-    latter_month_day = []
-    dr_month_day = []
-    for (month, day) in zip(former_months, former_days):
-        former_month_day.append(month*100+day)
-    for (month, day) in zip(latter_months, latter_days):
-        latter_month_day.append(month*100+day)
-    for (month, day) in zip(months, days):
-        dr_month_day.append(month*100+day)
-
-    for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
-
-        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
-
-        if idx == 0:
-            data = emissions_rates_df_out
-            df_cp = data
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_same = df_cp[df_cp['month_day'].isin(dr_month_day)]
-            df_former = df_cp[(df_cp['Report_Hour'] >= 12) & (df_cp['month_day'].isin(former_month_day))]
-            df_latter = df_cp[(df_cp['Report_Hour'] <= 12) & (df_cp['month_day'].isin(latter_month_day))]
-
-            df_same['new_hourID'] = df_same['Report_Hour']
-            df_former['new_hourID'] = df_former['Report_Hour'] - 24
-            df_latter['new_hourID'] = df_latter['Report_Hour'] + 24
-
-            frames = [df_same, df_former, df_latter]
-
-            df_all = pd.concat(frames)
-
-            "Compute daily average"
-            df_2[idx] = df_all.groupby(['new_hourID'])[column_name].mean().reset_index()
-
-        else:
-            pass
-
-    return df_2
+    "Compute daily average"
+    return df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
 
 
-def oldbins_all_year():
-    df_2 = {}
+def get_2022_hour_ave(data, time_df, column_name):
+    df_cp = data
 
     "Get days with DR hours"
-    df_temp_winter = \
-        DR_hours_df_dict_out['oldbins_Winter'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-    df_temp_winter = df_temp_winter[df_temp_winter['DVR'] >= 1]
-    df_temp_summer = \
-        DR_hours_df_dict_out['oldbins_Summer'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-    df_temp_summer = df_temp_summer[df_temp_summer['DVR'] >= 1]
-    # df_temp_winter['season'] = 'winter'
-    # df_temp_summer['season'] = 'summer'
-    frames = [df_temp_winter, df_temp_summer]
-    df_1 = pd.concat(frames)
+    df_1 = time_df.groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
+    df_1 = df_1[df_1['DVR'] >= 1]
 
-    "Get same, former and latter days with DR hours and put into list"
-    days = []
-    months = []
-    former_days = []
-    former_months = []
-    latter_days = []
-    latter_months = []
+    "Combine month and day together"
+    df_1['month_day'] = df_1['Month']*100+df_1['Day']
+    df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
 
-    for row in df_1.iterrows():
-        month = row[1]['Month']
-        day = row[1]['Day']
-        days.append(day)
-        months.append(month)
+    "Select DR days in emission rates dataset"
+    df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
+    df_2 = df_2[df_2['Report_Year'] == 2022]
 
-        if day-1 > 0:
-            former_day = day-1
-            former_month = month
-        else:
-            former_day = DAYS_IN_MONTH[month-2]
-            former_month = month-1
-
-        if day+1 <= DAYS_IN_MONTH[month-1]:
-            latter_day = day+1
-            latter_month = month
-        else:
-            latter_day = 1
-            latter_month = month+1
-
-        former_days.append(former_day)
-        former_months.append(former_month)
-        latter_days.append(latter_day)
-        latter_months.append(latter_month)
-
-    former_month_day = []
-    latter_month_day = []
-    dr_month_day = []
-    for (month, day) in zip(former_months, former_days):
-        former_month_day.append(month*100+day)
-    for (month, day) in zip(latter_months, latter_days):
-        latter_month_day.append(month*100+day)
-    for (month, day) in zip(months, days):
-        dr_month_day.append(month*100+day)
-
-    for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
-
-        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
-
-        if idx == 0:
-            data = emissions_rates_df_out
-            df_cp = data
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_same = df_cp[df_cp['month_day'].isin(dr_month_day)]
-            df_former = df_cp[(df_cp['Report_Hour'] >= 12) & (df_cp['month_day'].isin(former_month_day))]
-            df_latter = df_cp[(df_cp['Report_Hour'] <= 12) & (df_cp['month_day'].isin(latter_month_day))]
-
-            df_same['new_hourID'] = df_same['Report_Hour']
-            df_former['new_hourID'] = df_former['Report_Hour'] - 24
-            df_latter['new_hourID'] = df_latter['Report_Hour'] + 24
-
-            frames = [df_same, df_former, df_latter]
-
-            df_all = pd.concat(frames)
-
-            "Compute daily average"
-            df_2[idx] = df_all.groupby(['new_hourID'])[column_name].mean().reset_index()
-
-        else:
-            pass
-
-    return df_2
+    "Compute daily average"
+    return df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
 
 
 def new_bins_winter():
 
-    df_3 = {}
+    df_ave = {}
 
     for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
 
@@ -289,31 +212,19 @@ def new_bins_winter():
 
         if idx == 0:
             data = emissions_rates_df_out
-            df_cp = data
-
-            "Get days with DR hours"
-            df_1 = DR_hours_df_dict_out['newbins_Winter'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-            df_1 = df_1[df_1['DVR'] >= 1]
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
 
             "Compute daily average"
-            df_3[idx] = df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
+            df_ave[idx] = get_hour_ave(data, DR_hours_df_dict_out['newbins_Winter'], column_name)
 
         else:
             pass
 
-    return df_3
+    return df_ave
 
 
 def new_bins_summer():
 
-    df_3 = {}
+    df_ave = {}
 
     for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
 
@@ -321,31 +232,19 @@ def new_bins_summer():
 
         if idx == 0:
             data = emissions_rates_df_out
-            df_cp = data
-
-            "Get days with DR hours"
-            df_1 = DR_hours_df_dict_out['newbins_Summer'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-            df_1 = df_1[df_1['DVR'] >= 1]
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
 
             "Compute daily average"
-            df_3[idx] = df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
+            df_ave[idx] = get_hour_ave(data, DR_hours_df_dict_out['newbins_Summer'], column_name)
 
         else:
             pass
 
-    return df_3
+    return df_ave
 
 
 def new_bins_fall():
 
-    df_3 = {}
+    df_ave = {}
 
     for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
 
@@ -353,26 +252,14 @@ def new_bins_fall():
 
         if idx == 0:
             data = emissions_rates_df_out
-            df_cp = data
-
-            "Get days with DR hours"
-            df_1 = DR_hours_df_dict_out['newbins_Fall'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-            df_1 = df_1[df_1['DVR'] >= 1]
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
 
             "Compute daily average"
-            df_3[idx] = df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
+            df_ave[idx] = get_hour_ave(data, DR_hours_df_dict_out['newbins_Fall'], column_name)
 
         else:
             pass
 
-    return df_3
+    return df_ave
 
 
 def newbins_all_year():
@@ -461,99 +348,57 @@ def newbins_2022_allyear():
 
 
 def newbins_2022_winter():
-    df_3 = {}
+    df_2022_ave = {}
 
     for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
         column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
 
         if idx == 0:
             data = emissions_rates_df_out
-            df_cp = data
-
-            "Get days with DR hours"
-            df_1 = DR_hours_df_dict_out['newbins_Winter'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-            df_1 = df_1[df_1['DVR'] >= 1]
-            # df_1['season'] = 'winter'
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
-            df_2 = df_2[df_2['Report_Year'] == 2022]
 
             "Compute daily average"
-            df_3[idx] = df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
+            df_2022_ave[idx] = get_2022_hour_ave(data, DR_hours_df_dict_out['newbins_Winter'], column_name)
 
         else:
             pass
 
-    return df_3
+    return df_2022_ave
 
 
 def newbins_2022_summer():
-    df_3 = {}
+    df_2022_ave = {}
 
     for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
         column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
 
         if idx == 0:
             data = emissions_rates_df_out
-            df_cp = data
-
-            "Get days with DR hours"
-            df_1 = DR_hours_df_dict_out['newbins_Summer'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-            df_1 = df_1[df_1['DVR'] >= 1]
-            # df_1['season'] = 'summer'
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
-            df_2 = df_2[df_2['Report_Year'] == 2022]
 
             "Compute daily average"
-            df_3[idx] = df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
+            df_2022_ave[idx] = get_2022_hour_ave(data, DR_hours_df_dict_out['newbins_Summer'], column_name)
 
         else:
             pass
 
-    return df_3
+    return df_2022_ave
 
 
 def newbins_2022_fall():
-    df_3 = {}
+    df_2022_ave = {}
 
     for idx, file_name in enumerate(EMISSIONS_RATES_FILES):
         column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
 
         if idx == 0:
             data = emissions_rates_df_out
-            df_cp = data
-
-            "Get days with DR hours"
-            df_1 = DR_hours_df_dict_out['newbins_Fall'].groupby(['Month', 'Day'])['DVR', 'ResTOU'].sum().reset_index()
-            df_1 = df_1[df_1['DVR'] >= 1]
-            # df_1['season'] = 'fall'
-
-            "Combine month and day together"
-            df_1['month_day'] = df_1['Month']*100+df_1['Day']
-            df_cp['month_day'] = df_cp['Report_Month'] * 100 + df_cp['Report_Day']
-
-            "Select DR days in emission rates dataset"
-            df_2 = df_cp[df_cp['month_day'].isin(df_1['month_day'])]
-            df_2 = df_2[df_2['Report_Year'] == 2022]
 
             "Compute daily average"
-            df_3[idx] = df_2.groupby(['Report_Hour'])[column_name].mean().reset_index()
+            df_2022_ave[idx] = get_2022_hour_ave(data, DR_hours_df_dict_out['newbins_Fall'], column_name)
 
         else:
             pass
 
-    return df_3
+    return df_2022_ave
 
 
 def newbins_2022():
