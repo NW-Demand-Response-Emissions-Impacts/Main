d095908930cd1e7ab43fcb7b5a285ccf7571e326
Lily Caroline Hahn <lchahn@uw.edu>
2021-11-19 18:44:40 -0800

    pylint scores = 10 for emissions_parameters and subcomp_a

--- emissions_calculator/emissions_parameters.py
+++ emissions_calculator/emissions_parameters.py
@@ -1,13 +1,12 @@
 """
 emissions_parameters.py
 
-Defines directories, file names, and constants for use in the emissions_calculator and subcomponents.
-
+Defines directories, constants, and file names
+for use in the emissions_calculator and subcomponents.
 """
 
 # Import the os module
 import os
-import numpy as np
 
 # Get the Main folder directory, whether user is in Main/ or in Main/emissions_calculator/
 CWD = os.getcwd() #gives .../Main/emissions_calculator if run from emissions calculator folder
@@ -25,9 +24,11 @@ DIR_DATA_PROC = MAIN_FOLDER + '/processed_data/'
 # The emissions_calculator will run through the old and new bin data
 # Users can also just input one file each in DR_HRS_FILES and DR_POTENTIAL_FILES
 # Should move these notes to the user guide
-EMISSIONS_SCENARIO_LIST = ['Baseline'] #,'EarlyCoalRetirement','LimitedMarkets','NoGasBuildLimits','OrgMarkets','SCC']
-#discovered that these other scenarios have only monthly-mean hourly emissions rates, need to get more info so exclude for now
-EMISSIONS_RATES_FILES = ['AvoidedEmissionsRate' + x + '.xlsx' for x in EMISSIONS_SCENARIO_LIST]  
+EMISSIONS_SCENARIO_LIST = ['Baseline']
+#'EarlyCoalRetirement','LimitedMarkets','NoGasBuildLimits',\
+#'OrgMarkets','SCC']
+#exclude these other scenarios for now
+EMISSIONS_RATES_FILES = ['AvoidedEmissionsRate' + x + '.xlsx' for x in EMISSIONS_SCENARIO_LIST]
 DR_NAME = ['oldbins','newbins']
 DR_HRS_FILES = ['DRHours_' + x + '.xlsx' for x in DR_NAME]
 DR_POTENTIAL_FILES = ['DR RPM Inputs_071420.xlsx','DR RPM Inputs_021621_newaMWbins.xlsx']
@@ -37,5 +38,5 @@ SUBSET_PRODUCTS = [[0],['DVR','ResTOU']] #for newbins, only want to look at thes
 # Constants
 DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# multiply emissions rates in lbs CO2e/kWh by this factor to get metric tons CO2e/MWh 
+# factor*emissions rates in lbs CO2e/kWh = metric tons CO2e/MWh
 EMISSIONS_CHANGEUNITS = .4536
--- emissions_calculator/subcomp_a_organize_data.py
+++ emissions_calculator/subcomp_a_organize_data.py
@@ -1,143 +1,164 @@
 """
 subcomp_a_organize_data.py
 
-Reads input files with DR potential, DR implementation hours, and emissions factors, and creates data frames for each of these. 
+Reads input files with emissions factors,
+DR implementation hours, and DR potential.
 
-Also creates a product lookup dataframe that says which bin a product is in, and which DR_hours_label applies to the product. 
-
-This script will run through the old, standard DR bins, and also the new, sensitivity DR bins, both contained in DR_HRS_FILES and DR_POTENTIAL_FILES.
+Returns dataframe for emissions factors,
+and dictionaries of dataframes for DR hours and potential.
+Also returns a product lookup dataframe that gives the bin,
+seasonality, and shift/shed for each product.
 """
-# to do: add timing, exceptions, less hard-coding in the potential function if possible
+# to do: add timing, exceptions, testing
+# less hard-coding in the potential function if possible,
+# make so that file names are specified as main function input
+# rather than hard-coded into emissions_parameters.py
 
 import pandas as pd
-import numpy as np
 
-from emissions_parameters import DIR_EMISSIONS_RATES, \
+from emissions_calculator.emissions_parameters import DIR_EMISSIONS_RATES, \
     DIR_DR_POTENTIAL_HRS, EMISSIONS_SCENARIO_LIST, EMISSIONS_RATES_FILES, \
     DR_NAME, DR_HRS_FILES, DR_POTENTIAL_FILES, SUBSET_PRODUCTS, DR_SEASONS
-    
+
 def create_emissions_rates_df():
     """
-    Given a list of emissions rate files for different policy scenarios, 
-    creates a dataframe with 2022-2041 hourly emissions factors for all policy scenarios.
+    Reads in emissions rate files for different policy scenarios
+    to create a dataframe with 2022-2041 hourly emissions factors.
+
+    Returns:
+        emissions_rates_df: the emissions rates dataframe
     """
     sheet = 'HourlyAvoidedEmissionsRate'
     columns = ['Report_Year','Report_Month','Report_Day','Report_Hour', \
-               'Emissions Rate Estimate']   
-    
+               'Emissions Rate Estimate']
+
     for idx,file_name in enumerate(EMISSIONS_RATES_FILES):
-        
+
         xlsx = pd.ExcelFile(DIR_EMISSIONS_RATES+file_name)
-        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' Emissions Rate Estimate'
-        
-        #for first file, read in hours 
-        if idx == 0: 
-            df = pd.read_excel(xlsx,sheet,usecols=columns[0:4])  
+        column_name = EMISSIONS_SCENARIO_LIST[idx] + ' ' + columns[4]
+
+        #for first file, read in hours
+        if idx == 0:
+            df_o = pd.read_excel(xlsx,sheet,usecols=columns[0:4])
         else:
-            pass 
+            pass
 
         #for all files, add emissions rates to existing dataframe
-        df[column_name] = pd.read_excel(xlsx,sheet,usecols=[columns[4]])
-        
-    #subset year 2022-2041 #when testing make sure this matches year range for other vars
-    emissions_rates_df = df[df['Report_Year']>=2022]
-    
+        df_o[column_name] = pd.read_excel(xlsx,sheet,usecols=[columns[4]])
+
+    #subset year 2022-2041 #testing check this matches years for other vars
+    emissions_rates_df = df_o[df_o['Report_Year']>=2022]
+
     return emissions_rates_df
 
-def create_DR_hours_df_dict():
+def create_dr_hours_df_dict():
     """
-    Input: Excel files with 1 year of DR hours, where each file contains separate sheets for each season, 
-    and each sheet contains columns for the DR products in that season. 
-    Output: A dictionary of dataframes where each dataframe gives the DR hours for a given DR plan 
-    and season within that plan. 
-    """
-    DR_hours_df_dict = {} 
+    Reads in Excel files with 1 year of DR hours,
+    where each file contains separate sheets for each season,
+    and each sheet contains columns for the DR products in that season.
 
-    #when testing, should have same number of dr hrs files and dr potential files
+    Creates a dictionary of dataframes with each dataframe
+    corresponding to a given DR plan and season within that plan.
+
+    Returns:
+        dr_hours_df_dict: dictionary of DR hours dataframes
+    """
+    dr_hours_df_dict = {}
+
+    #when testing, should have same number of dr hrs and pot files
     for idx, file_name in enumerate(DR_HRS_FILES):
-        
+
         drname = DR_NAME[idx]
         seasons = DR_SEASONS[idx]
         xlsx = pd.ExcelFile(DIR_DR_POTENTIAL_HRS+file_name)
-        
+
         for season in seasons:
             dict_key = drname + '_' + season
-            DR_hours_df_dict[dict_key] = pd.read_excel(xlsx,season)
-        
-    return DR_hours_df_dict
-                           
+            dr_hours_df_dict[dict_key] = pd.read_excel(xlsx,season)
+
+    return dr_hours_df_dict
+
     #can access each dataframe using
-    #dr_hrs_dict = create_DR_hours_df()
+    #dr_hrs_dict = create_dr_hours_df()
     #for key in dr_hrs_dict.keys():
         #df = dr_hrs_dict[key]
-        
-def create_DR_potential_df_dict():
+
+def create_dr_potential_df_dict():
     """
-    Input: Excel files containing DR potential for each year 2022-2041 within each season.
-    Output: A dictionary of dataframes where each dataframe gives the potential for a given DR plan 
-    and season within that plan.
+    Reads in Excel files containing DR potential
+    for each year 2022-2041 with all seasons in the same sheet.
+
+    Creates a dictionary of dataframes with each dataframe
+    corresponding to a given DR plan and season within that plan.
+
+    Returns:
+        dr_pot_df_dict: dictionary of DR potential dataframes
     """
-    DR_potential_df_dict = {}
-    
+    dr_pot_df_dict = {}
+
     for idx, file_name in enumerate(DR_POTENTIAL_FILES):
-        
+
         drname = DR_NAME[idx]
         xlsx = pd.ExcelFile(DIR_DR_POTENTIAL_HRS+file_name)
-       
-        # in these files, summer and winter are in the same sheet, summer first and then winter
+
         # come back to make less hard-coded
         dict_key = drname + '_Summer'
-        DR_potential_df_dict[dict_key] = pd.read_excel(xlsx,'Reporter Outputs', \
-                                                       index_col=0,header=None,skiprows=1,nrows=21,usecols=range(21)).T
-        DR_potential_df_dict[dict_key] = DR_potential_df_dict[dict_key].rename(columns={'Product': 'Year'})
+        dr_pot_df_dict[dict_key] = pd.read_excel(xlsx,'Reporter Outputs', \
+            index_col=0,header=None,skiprows=1,nrows=21,usecols=range(21)).T
+        dr_pot_df_dict[dict_key] = dr_pot_df_dict[dict_key].rename(columns={'Product': 'Year'})
 
         dict_key = drname + '_Winter'
-        DR_potential_df_dict[dict_key] = pd.read_excel(xlsx,'Reporter Outputs', \
-                                                       index_col=0,header=None,skiprows=26,nrows=19,usecols=range(21)).T
-        DR_potential_df_dict[dict_key] = DR_potential_df_dict[dict_key].rename(columns={'Product': 'Year'})
+        dr_pot_df_dict[dict_key] = pd.read_excel(xlsx,'Reporter Outputs', \
+            index_col=0,header=None,skiprows=26,nrows=19,usecols=range(21)).T
+        dr_pot_df_dict[dict_key] = dr_pot_df_dict[dict_key].rename(columns={'Product': 'Year'})
 
-        #if only a subset of products is desired, e.g. for new bins just want DVR and ResTOU
-        subset = SUBSET_PRODUCTS[idx].copy() #copy because I don't want to change the parameter permanently
+        #if only a subset of products is desired, e.g. for new bins
+        subset = SUBSET_PRODUCTS[idx].copy()
         if isinstance(subset[0],str):
             subset.insert(0,'Year')
-            DR_potential_df_dict[drname + '_Summer'] = DR_potential_df_dict[drname + '_Summer'][subset]
-            DR_potential_df_dict[drname + '_Winter'] = DR_potential_df_dict[drname + '_Winter'][subset]
+            dr_pot_df_dict[drname + '_Summer'] = dr_pot_df_dict[drname + '_Summer'][subset]
+            dr_pot_df_dict[drname + '_Winter'] = dr_pot_df_dict[drname + '_Winter'][subset]
         else:
             pass
-        
+
         seasons = DR_SEASONS[idx]
-        if 'Fall' in seasons: #for new bins, winter potential is also applied to fall
-            DR_potential_df_dict[drname + '_Fall'] = DR_potential_df_dict[drname + '_Winter']
+        if 'Fall' in seasons: #for new bins, apply winter to fall
+            dr_pot_df_dict[drname + '_Fall'] = dr_pot_df_dict[drname + '_Winter']
         else:
             pass
-                    
-    return DR_potential_df_dict       
+
+    return dr_pot_df_dict
 
 def create_product_info_df():
     """
-    Input: Excel files containing DR potential for each year 2022-2041 within each season.
-    Output: A dictionary of dataframes for each DR plan, listing products, bins, seasonality, shift or shed 
+    Reads the DR potential file sheet with product data
+    and creates a dictionary of dataframes for each DR plan,
+    listing products, bins, seasonality, shift/shed.
+
+    Returns:
+        dr_product_info_df_dict: dictionary of DR product info dataframes
     """
-    DR_product_info_df_dict = {}
-    
+    dr_product_info_df_dict = {}
+
     for idx, file_name in enumerate(DR_POTENTIAL_FILES):
-        
+
         drname = DR_NAME[idx]
         xlsx = pd.ExcelFile(DIR_DR_POTENTIAL_HRS+file_name)
         column_names = ['Product','Bin','Seasonality','Shift or Shed?']
-        DR_product_info_df_dict[drname] = pd.read_excel(xlsx,'EnergyCalcs', \
-                                                       skiprows=2,nrows=23,usecols=column_names)  
-        
-    return DR_product_info_df_dict  
+        dr_product_info_df_dict[drname] = pd.read_excel(xlsx,'EnergyCalcs', \
+                                                       skiprows=2,nrows=23,usecols=column_names)
+
+    return dr_product_info_df_dict
 
 ################# Main ####################
 def runall():
     """
-    Runs through all of the above functions. 
+    Runs through all of the above functions.
     """
     emissions_rates_df_out = create_emissions_rates_df()
-    DR_hours_df_dict_out = create_DR_hours_df_dict()
-    DR_potential_df_dict_out = create_DR_potential_df_dict()
-    DR_product_info_df_dict_out = create_product_info_df()
-    
-    return emissions_rates_df_out, DR_hours_df_dict_out, DR_potential_df_dict_out, DR_product_info_df_dict_out            
+    dr_hours_df_dict_out = create_dr_hours_df_dict()
+    dr_potential_df_dict_out = create_dr_potential_df_dict()
+    dr_product_info_df_dict_out = create_product_info_df()
+
+    return emissions_rates_df_out, dr_hours_df_dict_out, \
+            dr_potential_df_dict_out, dr_product_info_df_dict_out
