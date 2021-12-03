fd1528c0bffb832fa4c38f4880b3b1f05a5d7142
EstherrrrLiu <changliues@gmail.com>
2021-12-02 22:43:54 -0800

    Added tests for subcomp_b

--- emissions_calculator/test_subcomp_b.py
+++ emissions_calculator/test_subcomp_b.py
@@ -2,10 +2,28 @@
 Tests for the sub component b
 """
 
+import pandas as pd
+import pandas.testing as pdt
 import unittest
 
 from subcomp_b_process_emissions_factors import seasonal_ave, annual_ave, \
-    newbins_2022_annual_ave, newbins_2022_seasonal_ave
+    get_hour_ave, alldays_oneyear_seasonal_ave, get_oneyear_hour_ave
+
+df_emissions_data = pd.read_excel('Test_data/emissions_data.xlsx')
+df_dr_hours_winter = pd.read_excel('Test_data/dr_hours_winter.xlsx')
+df_dr_hours_spring = pd.read_excel('Test_data/dr_hours_spring.xlsx')
+df_dr_hours_summer = pd.read_excel('Test_data/dr_hours_summer.xlsx')
+df_dr_hours_fall = pd.read_excel('Test_data/dr_hours_fall.xlsx')
+df_dr_hours_data = pd.read_excel('Test_data/dr_hours_data.xlsx')
+
+# For smoke test, define null input values
+# Just to make sure the function could run
+dr_name = {}
+dr_seasons = {}
+emissions_scenario_list = {}
+emissions_rates_df_out = {}
+dr_hours_df_dict_out = {}
+year = 2022
 
 
 class TestSubCompB(unittest.TestCase):
@@ -17,7 +35,55 @@ class TestSubCompB(unittest.TestCase):
         """
         Simple smoke test to make sure function runs.
         """
-        seasonal_ave()
-        annual_ave()
-        newbins_2022_annual_ave()
-        newbins_2022_seasonal_ave()
+        seasonal_ave(dr_name, dr_seasons, emissions_scenario_list,
+                     emissions_rates_df_out, dr_hours_df_dict_out)
+        annual_ave(dr_name, dr_seasons, emissions_scenario_list,
+                   emissions_rates_df_out, dr_hours_df_dict_out)
+        get_hour_ave(df_emissions_data, df_dr_hours_winter, 'Test Emissions Rate Estimate')
+        alldays_oneyear_seasonal_ave(emissions_scenario_list,
+                                     emissions_rates_df_out, year)
+        get_oneyear_hour_ave(df_emissions_data, 'Winter', 'Test Emissions Rate Estimate', year)
+
+    def test_get_hour_ave_winter(self):
+        """
+        One-shot test for get_hour_ave
+        Output is winter season average
+        """
+        df_output_dr_days_ave_winter = pd.read_excel('dr_days_ave_winter.xlsx')
+        df_dr_days_ave_winter = get_hour_ave(df_emissions_data, df_dr_hours_winter, 'Test Emissions Rate Estimate')
+        pdt.assert_frame_equal(df_output_dr_days_ave_winter, df_dr_days_ave_winter)
+
+    def test_get_hour_ave_spring(self):
+        """
+        One-shot test for get_hour_ave
+        Output is spring season average
+        """
+        df_output_dr_days_ave_spring = pd.read_excel('dr_days_ave_spring.xlsx')
+        df_dr_days_ave_spring = get_hour_ave(df_emissions_data, df_dr_hours_spring, 'Test Emissions Rate Estimate')
+        pdt.assert_frame_equal(df_output_dr_days_ave_spring, df_dr_days_ave_spring)
+
+    def test_get_hour_ave_annual(self):
+        """
+        One-shot test for get_hour_ave
+        Output is annual average
+        """
+        df_output_dr_days_ave_annual = pd.read_excel('dr_days_ave_annual.xlsx')
+        df_dr_days_ave_annual = get_hour_ave(df_emissions_data, df_dr_hours_data, 'Test Emissions Rate Estimate')
+        pdt.assert_frame_equal(df_output_dr_days_ave_annual, df_dr_days_ave_annual)
+
+    def test_get_oneyear_hour_ave_winter(self):
+        """
+        One-shot test for get_oneyear_hour_ave
+        Output is winter season average
+        """
+        df_output_all_days_ave_winter_2022 = pd.read_excel('all_days_ave_winter_2022.xlsx')
+        df_all_days_ave_winter_2022 = get_oneyear_hour_ave(df_emissions_data, 'Winter',
+                                                           'Test Emissions Rate Estimate', 2022)
+        pdt.assert_frame_equal(df_output_all_days_ave_winter_2022, df_all_days_ave_winter_2022)
+
+    def test_time_period_available(self):
+        """
+        Edge test to make sure input time period(season) in get_oneyear_hour_ave is defined.
+        """
+        with self.assertRaises(ValueError):
+            get_oneyear_hour_ave(df_emissions_data, 'Autumn', 'Test Emissions Rate Estimate', 2022)
